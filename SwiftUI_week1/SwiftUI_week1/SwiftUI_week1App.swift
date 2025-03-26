f8064d61291824a7c9d529b9c419a329fdc79371
YouBin <alicee0047@gmail.com>
2025-03-26 15:59:02 +0900

    💄 1주차 실습 완료
    1주차 실습 완료

---
+++ SwiftUI_week1/SwiftUI_week1/SwiftUI_week1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_week1App.swift
+//  SwiftUI_week1
+//
+//  Created by Youbin on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_week1App: App {
+    var body: some Scene {
+        WindowGroup {
+            TicketView()
+        }
+    }
+}
