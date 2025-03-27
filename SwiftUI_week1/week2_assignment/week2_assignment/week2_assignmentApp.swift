b42354a93cf7808be64195e3ba27b34c292db3a5
YouBin <alicee0047@gmail.com>
2025-03-27 20:18:27 +0900

    🌱 폴더 이름 변경 및 추가
    폴더 이름 변경 및 추가

---
+++ SwiftUI_week1/week2_assignment/week2_assignment/week2_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  week2_assignmentApp.swift
+//  week2_assignment
+//
+//  Created by Youbin on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct week2_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
